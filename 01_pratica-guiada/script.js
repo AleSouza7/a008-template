882261dfd0ce8301d2e6d7fc8a7d27aa343ab165
Alesouza7 <alexcostasouza86@yahoo.com>
2022-07-20 21:39:31 -0300

    Exercicios realizados aluno Alex souza

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,8 @@
-//CODIGO AQUI
+//CODIGO 
+const minhaidade=prompt("Qual a minha idade?")
+const amigoidade=prompt("Qual a sua idade amigo?")
+
+typeof console.log("Minha idade é maior que ao do meu amigo",minhaidade>amigoidade)
+console.log("Minha idade é",minhaidade,"anos. E a idade do meu amigo",amigoidade,"anos.")
+diferenca = minhaidade-amigoidade
+console.log("A diferença de idades é",diferenca,"anos")
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -7,3 +7,13 @@ const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+//Não sou maooir de idade e quero passear de carro
+//Sou maior de idade e vou para a prais, e não quero passear de carro
+//Vou para a praia ou quero passear de carro
+//Posso tomar uma cervejinha e não vou passear de carro
+
+!b && d
+(b && a) && !d
+a || d
+c && !d
